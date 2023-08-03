9fe6574731bb8e7b4d3f8ad27bd761ecd42b64a7
1LolaOla <funmiolay1@gmail.com>
2023-08-03 22:23:21 +0000

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+"""a number module for funnction 'floor'"""
 import math
 
-def floor(n: float) -> int:
 
-    return math.floor(n)
+def floor(n: float) -> int:
+    """a number function 'floor'"""
+    return math.floor(n))
