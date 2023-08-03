6af7283783e9935bb51f179328ba3cfd3c65913e
1LolaOla <funmiolay1@gmail.com>
2023-08-03 21:53:50 +0000

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenates two input strings and returns the resulting string.
-
-    Args:
-        str1 (str): The first input string.
-        str2 (str): The second input string.
-
     Returns:
         str: The concatenated string.
-    """
     return str1 + str2
 
